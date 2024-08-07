e2b7f6db7e45756f0e89c20a268b20f4580d7223
i-roger <mroger.dev@gmail.com>
2024-08-07 11:20:06 -0300

    Styling carousel and config

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,35 +1,60 @@
 export default function Carousel() {
   return (
-    <div style={carouselStyles.container} className="container">
-      <div style={carouselStyles.box} className="m4">...</div>
-      <div style={carouselStyles.box} className="box">...</div>
-      <div style={carouselStyles.box} className="box">...</div>
-      <div style={carouselStyles.box} className="box">...</div>
-      <div style={carouselStyles.box} className="box">...</div>
-      <div style={carouselStyles.box} className="box">...</div>
+    <div style={carouselStyles.container} className="">
+      <div>
+        <div style={carouselStyles.box} className="p-4">
+          IMAGE
+        </div>
+        <p className="text-black font-bold px-4 pt-2">Teste</p>
+      </div>
+      <div>
+        <div style={carouselStyles.box} className="p-4">
+          IMAGE
+        </div>
+        <p className="text-black font-bold px-4 pt-2">Teste</p>
+      </div>
+      <div>
+        <div style={carouselStyles.box} className="p-4">
+          IMAGE
+        </div>
+        <p className="text-black font-bold px-4 pt-2">Teste</p>
+      </div>
+      <div>
+        <div style={carouselStyles.box} className="p-4">
+          IMAGE
+        </div>
+        <p className="text-black font-bold px-4 pt-2">Teste</p>
+      </div>
+      <div>
+        <div style={carouselStyles.box} className="p-4">
+          IMAGE
+        </div>
+        <p className="text-black font-bold px-4 pt-2">Teste</p>
+      </div>
     </div>
   );
 }
 
 const carouselStyles = {
-    container: {
-        overflow: 'auto',
-        display: 'flex',
-        justifyCenter: 'center',
-        alignItems:'center',
-        scrollSnapType: 'x mandatory',
-        width: 200,
-        height: 300,
-
-    },
-    box: {
-        height: 200,
-        width: 200,
-        background: 'red',
-        marginRight: '5px',
-        flexShrink: 0,
-        scrollSnapAlign: 'start',
-        borderRadius: '30px'
-
-    }
-}
+  container: {
+    overflow: "auto",
+    display: "flex",
+    justifyCenter: "center",
+    alignItems: "center",
+    scrollSnapType: "x mandatory",
+    width: 250,
+    height: 300,
+  },
+  box: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 200,
+    width: 200,
+    background: 'rgb(24 24 27)',
+    marginRight: "5px",
+    flexShrink: 0,
+    scrollSnapAlign: "center",
+    borderRadius: "30px",
+  },
+};
