bc9b399f8072da5d1139aa8e46c63c69c0e879a2
iroger <mroger.dev@gmail.com>
2024-08-17 13:13:17 -0300

    Edit route.tsx auth for the same name on page.tsx login

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -13,8 +13,8 @@ const handler = NextAuth ({
             // e.g. domain, username, password, 2FA token, etc.
             // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
-              username: { label: "Username", type: "text", placeholder: "jsmith" },
-              password: { label: "Password", type: "password" }
+              username: { label: "username", type: "text"},
+              password: { label: "password", type: "password" }
             },
             async authorize(credentials, req) {
               console.log(credentials)
